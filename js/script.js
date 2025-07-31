04d652ed0987fed52f52971690fe9d2aa4220586
Rifat-Ullah-61 <it21061@mbstu.ac.bd>
2025-07-31 22:11:58 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+// Add active class to nav links on scroll
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav a');
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            const currentId = section.getAttribute('id');
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href') === `#${currentId}`) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+});
+
+const themeSwitcher = document.getElementById('theme-switcher');
+const body = document.body;
+
+// Check for saved theme preference
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    body.classList.add(currentTheme);
+}
+
+themeSwitcher.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    
+    let theme = 'light-mode';
+    if (body.classList.contains('dark-mode')) {
+        theme = 'dark-mode';
+    }
+    localStorage.setItem('theme', theme);
+});
